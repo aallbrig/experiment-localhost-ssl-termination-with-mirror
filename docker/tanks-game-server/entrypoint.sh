5a0838d21bb5c85222f0bf22d479bd1080eb56e2
Andrew Allbright <aallbrig@gmail.com>
2023-11-21 11:00:41 -0500

    WIP get tanks mirror game server accepting client connections

--- docker/tanks-game-server/entrypoint.sh
+++ docker/tanks-game-server/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
+GAME_SERVER_LINUX_ROOT=/root/game-server
+GAME_SERVER_LINUX_BINARY=tanks.game-server.x86_64
+GAME_SERVER_LINUX_BINARY_PATH=${GAME_SERVER_LINUX_ROOT}/${GAME_SERVER_LINUX_BINARY}
+
 function run_game_server() {
-  /root/game-server/tanks.game-server.x86_64 \
+  ${GAME_SERVER_LINUX_BINARY_PATH} \
       -batchmode \
       -nographics \
       -logfile /dev/stdout
