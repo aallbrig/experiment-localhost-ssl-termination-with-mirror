5e8db503908b1b7f23a4c0cbfb62e9e1574b0ed1
Andrew Allbright <aallbrig@gmail.com>
2023-11-24 17:28:32 -0500

    Add nginx configuration for the web game client

--- docker/tanks-game-server/entrypoint.sh
+++ docker/tanks-game-server/entrypoint.sh
@@ -2,10 +2,10 @@
 
 GAME_SERVER_LINUX_ROOT=/root/game-server
 GAME_SERVER_LINUX_BINARY=tanks.game-server.x86_64
-GAME_SERVER_LINUX_BINARY_PATH=${GAME_SERVER_LINUX_ROOT}/${GAME_SERVER_LINUX_BINARY}
+GAME_SERVER_LINUX_BINARY_PATH="${GAME_SERVER_LINUX_ROOT}/${GAME_SERVER_LINUX_BINARY}"
 
 function run_game_server() {
-  ${GAME_SERVER_LINUX_BINARY_PATH} \
+  "${GAME_SERVER_LINUX_BINARY_PATH}" \
       -batchmode \
       -nographics \
       -logfile /dev/stdout
